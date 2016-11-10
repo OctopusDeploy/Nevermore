b65499654ada3aff0ea0b8b916bcb5d5a69984e7
Robert Wagner <robert.wagner@octopus.com>
2016-11-10 11:57:08 +1000

    Added RelationalJsonContractResolver

---
+++ source/Nevermore/RelationalJsonContractResolver.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using Nevermore.Mapping;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System.Linq;
+
+namespace Nevermore
+{
+    public class RelationalJsonContractResolver : DefaultContractResolver
+    {
+        private RelationalMappings mappings;
+
+        public RelationalJsonContractResolver(RelationalMappings mappings)
+        {
+            this.mappings = mappings;
+        }
+
+        protected override JsonProperty CreateProperty(MemberInfo member, MemberSerialization memberSerialization)
+        {
+            DocumentMap map;
+            mappings.TryGet(member.DeclaringType, out map);
+
+            var property = base.CreateProperty(member, memberSerialization);
+
+            // ID properties are stored as columns
+            if (property.PropertyName == "Id" && map != null)
+            {
+                property.Ignored = true;
+            }
+
+            // Indexed properties are stored as columns
+            if (map != null && map.IndexedColumns.Any(c => c.Property != null && c.Property.Name == member.Name))
+            {
+                property.Ignored = true;
+            }
+
+            if (!property.Writable)
+            {
+                var property2 = member as PropertyInfo;
+                if (property2 != null)
+                {
+                    var hasPrivateSetter = property2.GetSetMethod(true) != null;
+                    property.Writable = hasPrivateSetter;
+                }
+            }
+
+            return property;
+        }
+    }
+}
