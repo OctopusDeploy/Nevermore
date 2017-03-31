98e71271ec13e6e27c9f6874b145cc41071902bb
John Simons <john.simons@octopus.com>
2017-03-31 17:31:43 +1000

    Update dependencies

--- source/Nevermore.Contracts/Properties/AssemblyInfo.cs
+++ source/Nevermore.Contracts/Properties/AssemblyInfo.cs
@@ -4,10 +4,10 @@
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Nevermore.Contracts")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("A JSON Document Store library for SQL Server")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Nevermore.Contracts")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
