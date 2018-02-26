f3ac54e65ecc219df6c3da4fac60fba4fa1e5873
Robert Wagner <robert.wagner@octopus.com>
2018-02-26 11:21:32 +1000

    Added In, Contains, StartsWith and EndsWith

---
+++ source/Nevermore.Tests/Linq/LinqWhereClauseMethodOperationTests.cs
@@ -0,0 +1,111 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Nevermore.Tests.Query;
+using Xunit;
+
+namespace Nevermore.Tests.Linq
+{
+    public class LinqWhereClauseMethodOperationTests : LinqTestBase
+    {
+        [Fact]
+        public void StringContains()
+        {
+            var builder = NewQueryBuilder();
+
+            var result = builder.Where(b => b.String.Contains("Bar"));
+
+            AssertLikeResult(result, "%Bar%");
+        }
+
+        [Fact]
+        public void StartsWith()
+        {
+            var builder = NewQueryBuilder();
+
+            var result = builder.Where(b => b.String.StartsWith("Bar"));
+
+            AssertLikeResult(result, "Bar%");
+        }
+        
+        [Fact]
+        public void EndsWith()
+        {
+            var builder = NewQueryBuilder();
+
+            var result = builder.Where(b => b.String.EndsWith("Bar"));
+
+            AssertLikeResult(result, "%Bar");
+        }
+        
+        [Fact]
+        public void ArrayContains()
+        {
+            var builder = NewQueryBuilder();
+
+            var values = new[] {1, 2, 3};
+            var result = builder.Where(b => values.Contains(b.Int));
+
+            AssertContainsResult(result);
+        }
+
+     
+
+        [Fact]
+        public void IListArrayContains()
+        {
+            var builder = NewQueryBuilder();
+
+            IList<int> values = new[] {1, 2, 3};
+            var result = builder.Where(b => values.Contains(b.Int));
+
+            AssertContainsResult(result);
+        }
+        
+        [Fact]
+        public void IReadOnlyListArrayContains()
+        {
+            var builder = NewQueryBuilder();
+
+            IReadOnlyList<int> values = new[] {1, 2, 3};
+            var result = builder.Where(b => values.Contains(b.Int));
+
+            AssertContainsResult(result);
+        }
+        
+        [Fact]
+        public void ListContains()
+        {
+            var builder = NewQueryBuilder();
+
+            var values = new List<int> {1, 2, 3};
+            var result = builder.Where(b => values.Contains(b.Int));
+
+            AssertContainsResult(result);
+        }
+
+        
+        static void AssertLikeResult(IQueryBuilder<Foo> result, string expected)
+        {
+            result.DebugViewRawQuery()
+                .Should()
+                .Be("SELECT * FROM dbo.[Foo] WHERE ([String] LIKE @string) ORDER BY [Id]");
+
+            result.QueryGenerator.QueryParameters.Single().Key.Should().Be("string");
+            result.QueryGenerator.QueryParameters.Should().Contain("string", expected);
+        }
+        
+        static void AssertContainsResult(IQueryBuilder<Foo> result)
+        {
+            result.DebugViewRawQuery()
+                .Should()
+                .Be("SELECT * FROM dbo.[Foo] WHERE ([Int] IN (@int0, @int1, @int2)) ORDER BY [Id]");
+
+            result.QueryGenerator.QueryParameters.Should().Contain("int0", "1");
+            result.QueryGenerator.QueryParameters.Should().Contain("int1", "2");
+            result.QueryGenerator.QueryParameters.Should().Contain("int2", "3");
+        }
+
+ 
+    }
+}
--- source/Nevermore/QueryBuilderWhereExtensions.cs
+++ source/Nevermore/QueryBuilderWhereExtensions.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
@@ -14,33 +15,93 @@ namespace Nevermore
             where TRecord : class
             => AddWhereClauseFromExpression(queryBuilder, predicate.Body);
 
+        
         static IQueryBuilder<TRecord> AddWhereClauseFromExpression<TRecord>(IQueryBuilder<TRecord> queryBuilder, Expression expr)
             where TRecord : class
         {
-            if (!(expr is BinaryExpression binExpr))
-                throw new NotSupportedException(
-                    $"The predicate supplied is not supported. Only simple BinaryExpressions and LogicalBinaryExpressions are supported. The predicate is a {expr.GetType()}.");
-
-            switch (binExpr.NodeType)
+            if (expr is BinaryExpression binExpr)
             {
-                case ExpressionType.Equal:
-                    return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.Equal, binExpr);
-                case ExpressionType.GreaterThan:
-                    return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.GreaterThan, binExpr);
-                case ExpressionType.GreaterThanOrEqual:
-                    return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.GreaterThanOrEqual, binExpr);
-                case ExpressionType.LessThan:
-                    return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.LessThan, binExpr);
-                case ExpressionType.LessThanOrEqual:
-                    return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.LessThanOrEqual, binExpr);
-                case ExpressionType.NotEqual:
-                    return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.NotEqual, binExpr);
-                case ExpressionType.AndAlso:
-                    return AddLogicalAndOperatorWhereClauses(queryBuilder, binExpr);
+                switch (binExpr.NodeType)
+                {
+                    case ExpressionType.Equal:
+                        return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.Equal, binExpr);
+                    case ExpressionType.GreaterThan:
+                        return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.GreaterThan, binExpr);
+                    case ExpressionType.GreaterThanOrEqual:
+                        return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.GreaterThanOrEqual, binExpr);
+                    case ExpressionType.LessThan:
+                        return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.LessThan, binExpr);
+                    case ExpressionType.LessThanOrEqual:
+                        return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.LessThanOrEqual, binExpr);
+                    case ExpressionType.NotEqual:
+                        return AddUnaryWhereClauseFromExpression(queryBuilder, SqlOperand.NotEqual, binExpr);
+                    case ExpressionType.AndAlso:
+                        return AddLogicalAndOperatorWhereClauses(queryBuilder, binExpr);
+                    default:
+                        throw new NotSupportedException($"The operand {binExpr.NodeType.ToString()} is not supported.");
+                }
+            }
+
+            if (expr is MethodCallExpression methExpr)
+            {
+                if(methExpr.Arguments.Count == 1 && methExpr.Arguments[0].Type == typeof(string))
+                    return AddStringMethodFromExpression(queryBuilder, methExpr);
+                
+                if(methExpr.Method.Name == "Contains")
+                    return AddContainsFromExpression(queryBuilder, methExpr);
+                
+                throw new NotSupportedException("Only method calls that take a single string argument and Enumerable.Contains methods are supported");
+            }
+
+            throw new NotSupportedException($"The predicate supplied is not supported. Only simple BinaryExpressions, LogicalBinaryExpressions and some MethodCallExpressions are supported. The predicate is a {expr.GetType()}.");
+
+        }
+
+        static IQueryBuilder<TRecord> AddContainsFromExpression<TRecord>(IQueryBuilder<TRecord> queryBuilder, MethodCallExpression methExpr)
+            where TRecord : class
+        {
+            var property = GetProperty(methExpr.Arguments.Count == 1 ? methExpr.Arguments[0] : methExpr.Arguments[1]);
+            var value = GetValueFromExpression(methExpr.Arguments.Count == 1 ? methExpr.Object : methExpr.Arguments[0], property.PropertyType);
+
+            return queryBuilder.Where(property.Name, SqlOperand.In, value);
+        }
+        
+
+        static IQueryBuilder<TRecord> AddStringMethodFromExpression<TRecord>(IQueryBuilder<TRecord> queryBuilder, MethodCallExpression methExpr) 
+            where TRecord : class
+        {
+            var property = GetProperty(methExpr.Object);
+            var fieldName = property.Name;
+
+            var value = (string) GetValueFromExpression(methExpr.Arguments[0], typeof(string));
+
+            switch (methExpr.Method.Name)
+            {
+                case "Contains":
+                    return queryBuilder.Where(fieldName, SqlOperand.Contains, value);
+                case "StartsWith":
+                    return queryBuilder.Where(fieldName, SqlOperand.StartsWith, value);
+                case "EndsWith":
+                    return queryBuilder.Where(fieldName, SqlOperand.EndsWith, value);
                 default:
-                    throw new NotSupportedException($"The operand {binExpr.NodeType.ToString()} is not supported.");
+                    throw new NotSupportedException($"The method {methExpr.Method.Name} is not supported. Only Contains, StartWith and EndsWith is supported");
             }
         }
+
+
+        static PropertyInfo GetProperty(Expression expression)
+        {
+            if (expression is UnaryExpression unaryExpr)
+                expression = unaryExpr.Operand;
+
+            if (expression is MemberExpression memberExpr && memberExpr.Member is PropertyInfo pi)
+                return pi;
+
+            throw new NotSupportedException(
+                $"The left hand side of the predicate must be a property accessor (PropertyExpression or UnaryExpression). It is a {(expression?.GetType().Name ?? "<unknown>")}.");
+        }
+        
+        
 
         static IQueryBuilder<TRecord> AddLogicalAndOperatorWhereClauses<TRecord>(IQueryBuilder<TRecord> queryBuilder, BinaryExpression binExpr)
             where TRecord : class
@@ -52,23 +113,10 @@ namespace Nevermore
         static IQueryBuilder<TRecord> AddUnaryWhereClauseFromExpression<TRecord>(IQueryBuilder<TRecord> queryBuilder, SqlOperand operand, BinaryExpression binaryExpression)
             where TRecord : class
         {
-            PropertyInfo GetLeft()
-            {
-                var leftExpr = binaryExpression.Left;
-                if (binaryExpression.Left is UnaryExpression unaryExpr)
-                    leftExpr = unaryExpr.Operand;
+            var property = GetProperty(binaryExpression.Left);
 
-                if (leftExpr is MemberExpression memberExpr && memberExpr.Member is PropertyInfo pi)
-                    return pi;
-
-                throw new NotSupportedException(
-                    $"The left hand side of the predicate must be a property accessor (PropertyExpression or UnaryExpression). It is a {binaryExpression.Left.GetType()}.");
-            }
-
-            var left = GetLeft();
-
-            var value = GetValueFromExpression(binaryExpression.Right, left.PropertyType);
-            var fieldName = left.Name;
+            var value = GetValueFromExpression(binaryExpression.Right, property.PropertyType);
+            var fieldName = property.Name;
             return queryBuilder.Where(fieldName, operand, value);
         }
 
