c2f5fa3d1cdfc151ea8d43336b664bba49aaf7e7
Tom Peters <tom.peters@octopus.com>
2018-03-14 16:12:51 +1000

    Actually delete the deprecated SqlOperand enum type

--- source/Nevermore/QueryBuilderWhereExtensions.cs
+++ source/Nevermore/QueryBuilderWhereExtensions.cs
@@ -1,28 +1,11 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 
 namespace Nevermore
 {
-    public enum SqlOperand
-    {
-        Equal,
-        In,
-        StartsWith,
-        EndsWith,
-        Between,
-        BetweenOrEqual,
-        GreaterThan,
-        GreaterThanOrEqual,
-        LessThan,
-        LessThanOrEqual,
-        NotEqual,
-        Contains
-    }
-    
     public static class QueryBuilderWhereExtensions
     {
         /// <summary>
