f66ab2b0fe9ef5b1089acfa993a50c742490e941
Tom Peters <tom.peters@octopus.com>
2018-03-16 13:30:03 +1000

    Added XML doc to the new transaction extension method.

--- source/Nevermore/RelationalTransactionExtensions.cs
+++ source/Nevermore/RelationalTransactionExtensions.cs
@@ -4,6 +4,11 @@ namespace Nevermore
 {
     public static class RelationalTransactionExtensions
     {
+        /// <summary>
+        /// Creates a query that returns strongly typed documents.
+        /// </summary>
+        /// <typeparam name="TDocument">The type of document being queried. Results from the database will be mapped to this type.</typeparam>
+        /// <returns>A stream of resulting documents.</returns>
         public static IQueryBuilder<TDocument> Query<TDocument>(this IRelationalTransaction transaction) where TDocument : class, IId
         {
             return transaction.TableQuery<TDocument>()
