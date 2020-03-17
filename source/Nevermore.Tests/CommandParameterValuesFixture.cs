d29d0974670575b077f477fd676abeb3f2967a0c
Adam McCoy <adam.mccoy@octopus.com>
2020-03-17 12:51:36 +1100

    Add test to verify replacements work across line breaks

--- source/Nevermore.Tests/CommandParameterValuesFixture.cs
+++ source/Nevermore.Tests/CommandParameterValuesFixture.cs
@@ -1,5 +1,6 @@
-using Microsoft.Data.SqlClient;
+using System;
 using FluentAssertions;
+using Microsoft.Data.SqlClient;
 using NUnit.Framework;
 
 namespace Nevermore.Tests
@@ -17,6 +18,19 @@ namespace Nevermore.Tests
             parameters.ContributeTo(command);
 
             command.CommandText.Should().Be("SELECT * FROM [Table] WHERE [Id] IN (@someId_0, @someId_1) AND [OtherId] = @someIdentifier");
+        }
+
+        [Test]
+        public void ShouldReplaceParametersAcrossLineBreaks()
+        {
+            var parameters = new CommandParameterValues();
+            parameters.Add("someId", new[] { "id1", "id2" });
+            parameters.Add("someIdentifier", "value");
+            var command = new SqlCommand($"SELECT * FROM [Table]{Environment.NewLine}WHERE [Id] IN @someId{Environment.NewLine}AND [OtherId] = @someIdentifier");
+
+            parameters.ContributeTo(command);
+
+            command.CommandText.Should().Be($"SELECT * FROM [Table]{Environment.NewLine}WHERE [Id] IN (@someId_0, @someId_1){Environment.NewLine}AND [OtherId] = @someIdentifier");
         }
     }
 }
