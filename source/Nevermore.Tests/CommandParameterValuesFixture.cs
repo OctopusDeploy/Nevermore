1932a3696ddfd878b1dd671bbcdcb1cfe0052e2f
Adam McCoy <adam.mccoy@octopus.com>
2020-03-17 16:51:48 +1100

    Add test to verify list replacement works when it's at the end of the query text

--- source/Nevermore.Tests/CommandParameterValuesFixture.cs
+++ source/Nevermore.Tests/CommandParameterValuesFixture.cs
@@ -22,6 +22,22 @@ namespace Nevermore.Tests
             command.Parameters["someId_1"].Value.Should().Be("id2");
             command.Parameters["someIdentifier"].Value.Should().Be("value");
         }
+        
+        [Test]
+        public void ShouldReplaceParametersWithExactMatchEndOfQuery()
+        {
+            var parameters = new CommandParameterValues();
+            parameters.Add("someId", "value");
+            parameters.Add("someIdentifier", new[] { "id1", "id2" });
+            var command = new SqlCommand("SELECT * FROM [Table] WHERE [Id] = @someId AND [OtherId] IN @someIdentifier");
+
+            parameters.ContributeTo(command);
+
+            command.CommandText.Should().Be("SELECT * FROM [Table] WHERE [Id] = @someId AND [OtherId] IN (@someIdentifier_0, @someIdentifier_1)");
+            command.Parameters["someId"].Value.Should().Be("value");
+            command.Parameters["someIdentifier_0"].Value.Should().Be("id1");
+            command.Parameters["someIdentifier_1"].Value.Should().Be("id2");
+        }
 
         [TestCase("@")]
         [TestCase("#")]
