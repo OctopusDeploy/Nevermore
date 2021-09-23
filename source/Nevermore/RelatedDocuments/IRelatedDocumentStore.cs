53d2119f51a8e572d0f67e3d013ff7daec2d49a9
Adam McCoy <adam.mccoy@octopus.com>
2021-09-23 16:01:30 +1000

    Naming tweak

--- source/Nevermore/RelatedDocuments/IRelatedDocumentStore.cs
+++ source/Nevermore/RelatedDocuments/IRelatedDocumentStore.cs
@@ -7,8 +7,8 @@ namespace Nevermore.RelatedDocuments
     public interface IRelatedDocumentStore
     {
         void PopulateRelatedDocuments<TDocument>(IWriteTransaction transaction, TDocument instance) where TDocument : class;
-        void PopulateManyRelatedDocuments<TDocument>(IWriteTransaction transaction, IEnumerable<TDocument> instance) where TDocument : class;
+        void PopulateManyRelatedDocuments<TDocument>(IWriteTransaction transaction, IEnumerable<TDocument> instances) where TDocument : class;
         Task PopulateRelatedDocumentsAsync<TDocument>(IWriteTransaction transaction, TDocument instance, CancellationToken cancellationToken = default) where TDocument : class;
-        Task PopulateManyRelatedDocumentsAsync<TDocument>(IWriteTransaction transaction, IEnumerable<TDocument> instance, CancellationToken cancellationToken = default) where TDocument : class;
+        Task PopulateManyRelatedDocumentsAsync<TDocument>(IWriteTransaction transaction, IEnumerable<TDocument> instances, CancellationToken cancellationToken = default) where TDocument : class;
     }
 }
