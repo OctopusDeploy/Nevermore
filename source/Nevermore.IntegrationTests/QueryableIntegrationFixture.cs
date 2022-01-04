6a73f96eeda05470c58b3d4b7e9ce50804263aaf
Adam McCoy <adam.mccoy@octopus.com>
2022-01-04 12:43:48 +1100

    More tests

--- source/Nevermore.IntegrationTests/QueryableIntegrationFixture.cs
+++ source/Nevermore.IntegrationTests/QueryableIntegrationFixture.cs
@@ -35,6 +35,31 @@ namespace Nevermore.IntegrationTests
         }
 
         [Test]
+        public void First()
+        {
+            using var t = Store.BeginTransaction();
+
+            var testCustomers = new[]
+            {
+                new Customer { FirstName = "Alice", LastName = "Apple" },
+                new Customer { FirstName = "Bob", LastName = "Banana" },
+                new Customer { FirstName = "Charlie", LastName = "Cherry" }
+            };
+
+            foreach (var c in testCustomers)
+            {
+                t.Insert(c);
+            }
+
+            t.Commit();
+
+            var customer = t.Queryable<Customer>()
+                .First();
+
+            customer.LastName.Should().BeEquivalentTo("Apple");
+        }
+
+        [Test]
         public void FirstWithPredicate()
         {
             using var t = Store.BeginTransaction();
@@ -57,6 +82,202 @@ namespace Nevermore.IntegrationTests
                 .FirstOrDefault(c => c.FirstName == "Alice");
 
             customer.LastName.Should().BeEquivalentTo("Apple");
+        }
+
+        [Test]
+        public void Skip()
+        {
+            using var t = Store.BeginTransaction();
+
+            var testCustomers = new[]
+            {
+                new Customer { FirstName = "Alice", LastName = "Apple" },
+                new Customer { FirstName = "Bob", LastName = "Banana" },
+                new Customer { FirstName = "Charlie", LastName = "Cherry" }
+            };
+
+            foreach (var c in testCustomers)
+            {
+                t.Insert(c);
+            }
+
+            t.Commit();
+
+            var customers = t.Queryable<Customer>()
+                .Skip(2)
+                .ToList();
+
+            customers.Select(c => c.LastName).Should().BeEquivalentTo("Cherry");
+        }
+
+        [Test]
+        public void Take()
+        {
+            using var t = Store.BeginTransaction();
+
+            var testCustomers = new[]
+            {
+                new Customer { FirstName = "Alice", LastName = "Apple" },
+                new Customer { FirstName = "Bob", LastName = "Banana" },
+                new Customer { FirstName = "Charlie", LastName = "Cherry" }
+            };
+
+            foreach (var c in testCustomers)
+            {
+                t.Insert(c);
+            }
+
+            t.Commit();
+
+            var customers = t.Queryable<Customer>()
+                .Take(2)
+                .ToList();
+
+            customers.Select(c => c.LastName).Should().BeEquivalentTo("Apple", "Banana");
+        }
+
+        [Test]
+        public void Count()
+        {
+            using var t = Store.BeginTransaction();
+
+            var testCustomers = new[]
+            {
+                new Customer { FirstName = "Alice", LastName = "Apple" },
+                new Customer { FirstName = "Bob", LastName = "Banana" },
+                new Customer { FirstName = "Charlie", LastName = "Cherry" }
+            };
+
+            foreach (var c in testCustomers)
+            {
+                t.Insert(c);
+            }
+
+            t.Commit();
+
+            var count = t.Queryable<Customer>().Count();
+
+            count.Should().Be(3);
+        }
+
+        [Test]
+        public void CountWithPredicate()
+        {
+            using var t = Store.BeginTransaction();
+
+            var testCustomers = new[]
+            {
+                new Customer { FirstName = "Alice", LastName = "Apple", Nickname = "Bandit" },
+                new Customer { FirstName = "Bob", LastName = "Banana", Nickname = "Chief" },
+                new Customer { FirstName = "Charlie", LastName = "Cherry", Nickname = "Cherry Bomb" }
+            };
+
+            foreach (var c in testCustomers)
+            {
+                t.Insert(c);
+            }
+
+            t.Commit();
+
+            var count = t.Queryable<Customer>().Count(c => c.Nickname.StartsWith("C"));
+
+            count.Should().Be(2);
+        }
+
+        [Test]
+        public void OrderBy()
+        {
+            using var t = Store.BeginTransaction();
+
+            var testCustomers = new[]
+            {
+                new Customer { FirstName = "Alice", LastName = "Apple", Nickname = "Zeta" },
+                new Customer { FirstName = "Bob", LastName = "Banana", Nickname = "Alpha" },
+                new Customer { FirstName = "Charlie", LastName = "Cherry", Nickname = "Omega" }
+            };
+
+            foreach (var c in testCustomers)
+            {
+                t.Insert(c);
+            }
+
+            t.Commit();
+
+            var customers = t.Queryable<Customer>().OrderBy(c => c.Nickname).ToList();
+
+            customers.Select(c => c.LastName).Should().BeEquivalentTo("Banana", "Cherry", "Apple");
+        }
+
+        [Test]
+        public void OrderByDescending()
+        {
+            using var t = Store.BeginTransaction();
+
+            var testCustomers = new[]
+            {
+                new Customer { FirstName = "Alice", LastName = "Apple", Nickname = "Omega" },
+                new Customer { FirstName = "Bob", LastName = "Banana", Nickname = "Alpha" },
+                new Customer { FirstName = "Charlie", LastName = "Cherry", Nickname = "Zeta" }
+            };
+
+            foreach (var c in testCustomers)
+            {
+                t.Insert(c);
+            }
+
+            t.Commit();
+
+            var customers = t.Queryable<Customer>().OrderByDescending(c => c.Nickname).ToList();
+
+            customers.Select(c => c.LastName).Should().BeEquivalentTo("Cherry", "Apple", "Banana");
+        }
+
+        [Test]
+        public void Any()
+        {
+            using var t = Store.BeginTransaction();
+
+            var testCustomers = new[]
+            {
+                new Customer { FirstName = "Alice", LastName = "Apple", Nickname = "Omega" },
+                new Customer { FirstName = "Bob", LastName = "Banana", Nickname = "Alpha" },
+                new Customer { FirstName = "Charlie", LastName = "Cherry", Nickname = "Zeta" }
+            };
+
+            foreach (var c in testCustomers)
+            {
+                t.Insert(c);
+            }
+
+            t.Commit();
+
+            var anyCustomers = t.Queryable<Customer>().Any();
+
+            anyCustomers.Should().BeTrue();
+        }
+
+        [Test]
+        public void AnyWithPredicate()
+        {
+            using var t = Store.BeginTransaction();
+
+            var testCustomers = new[]
+            {
+                new Customer { FirstName = "Alice", LastName = "Apple", Nickname = "Omega" },
+                new Customer { FirstName = "Bob", LastName = "Banana", Nickname = "Alpha" },
+                new Customer { FirstName = "Charlie", LastName = "Cherry", Nickname = "Zeta" }
+            };
+
+            foreach (var c in testCustomers)
+            {
+                t.Insert(c);
+            }
+
+            t.Commit();
+
+            var anyCustomers = t.Queryable<Customer>().Any(c => c.Nickname == "Warlock");
+
+            anyCustomers.Should().BeFalse();
         }
     }
 }
