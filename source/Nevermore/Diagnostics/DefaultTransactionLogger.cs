a9843d64c81a3a581f795306c72631e9363a2106
veo.chen <veo.chen@octopus.com>
2023-05-10 10:55:07 +1200

    log nothing in DefaultTransactionLogger

--- source/Nevermore/Diagnostics/DefaultTransactionLogger.cs
+++ source/Nevermore/Diagnostics/DefaultTransactionLogger.cs
@@ -1,22 +1,9 @@
-﻿using Nevermore.Diagnositcs;
-
-namespace Nevermore.Diagnostics
+﻿namespace Nevermore.Diagnostics
 {
     public class DefaultTransactionLogger : ITransactionLogger
     {
-        static readonly ILog Log = LogProvider.For<DefaultTransactionLogger>();
-
-        readonly long infoThreshold;
-
-        public DefaultTransactionLogger(long infoThreshold = 10_000)
-        {
-            this.infoThreshold = infoThreshold;
-        }
-        
         public void Write(long duration, string transactionName)
         {
-            var level = duration >= infoThreshold ? LogLevel.Info : LogLevel.Debug;
-            Log.Log(level, () => $"Transaction '{transactionName}' was open for {duration}ms");
         }
     }
 }
