52c1fb8d17e223283cbd8f2793633d1c70300e60
Eoin Motherway <25342760+YuKitsune@users.noreply.github.com>
2023-07-12 11:03:21 +1000

    Ignore non-IQueryable methods

--- source/Nevermore.Analyzers/NevermoreQueryableAnalyzer.cs
+++ source/Nevermore.Analyzers/NevermoreQueryableAnalyzer.cs
@@ -27,12 +27,21 @@ public class NevermoreQueryableAnalyzer : DiagnosticAnalyzer
         {
             return;
         }
+        
+        // Only target IQueryable methods
+        var symbolInfo = ModelExtensions.GetSymbolInfo(context.SemanticModel, memberAccessExpressionSyntax);
+        if (symbolInfo.Symbol is not IMethodSymbol methodSymbol ||
+            !IsQueryableExtensionMethod(methodSymbol))
+        {
+            return;
+        }
 
         // Only target calls to methods on Nevermore types that return an IQueryable.
-        // E.g: IReadQueryExecutor.Queryable<T>()
-        var symbolInfo = ModelExtensions.GetSymbolInfo(context.SemanticModel, memberAccessExpressionSyntax.Expression);
-        if (symbolInfo.Symbol is not IMethodSymbol methodSymbol || 
-            !methodSymbol.ContainingType.ContainingNamespace.Name.StartsWith("Nevermore"))
+        // E.g: IReadQueryExecutor.Queryable<T>().Select()
+        //  not IQueryable<string> GetNames(IQueryable<T> queryable) => queryable.Select(c => c.Names)
+        var parentSymbolInfo = ModelExtensions.GetSymbolInfo(context.SemanticModel, memberAccessExpressionSyntax.Expression);
+        if (parentSymbolInfo.Symbol is not IMethodSymbol parentMethodSymbol || 
+            !parentMethodSymbol.ContainingType.ContainingNamespace.Name.StartsWith("Nevermore"))
             return;
 
         var result = invocationExpressionSyntax.Accept(new NevermoreQueryableVisitor());
@@ -44,5 +53,21 @@ public class NevermoreQueryableAnalyzer : DiagnosticAnalyzer
                     result.Location ?? invocationExpressionSyntax.GetLocation(),
                     result.Message));
         }
+    }
+
+    bool IsQueryableExtensionMethod(IMethodSymbol methodSymbol)
+    {
+        // If the method is not defined in the Queryable class, then we don't care
+        if (methodSymbol.ContainingType.Name != nameof(Queryable))
+            return false;
+            
+        // If this is an extension method that doesn't take an IQueryable as it's receiver, then we don't care
+        if (methodSymbol is {IsExtensionMethod: true, ReceiverType: not null} &&
+            methodSymbol.ReceiverType.Interfaces.All(i => i.Name != nameof(IQueryable)))
+        {
+            return false;
+        }
+
+        return true;
     }
 }
