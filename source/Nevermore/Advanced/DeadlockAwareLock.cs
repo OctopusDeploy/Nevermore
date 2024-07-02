dba4ab4aa8d249376768fd1e05a46642c9c2fd68
Nick Roscarel <169005170+nick-roscarel-octo@users.noreply.github.com>
2024-07-02 11:15:06 +1000

    Remove broken deadlock awareness

--- source/Nevermore/Advanced/DeadlockAwareLock.cs
+++ source/Nevermore/Advanced/DeadlockAwareLock.cs
@@ -32,8 +32,6 @@ namespace Nevermore.Advanced
 
         public void Wait()
         {
-            AssertNoDeadlock();
-
             // `SemaphoreSlim` counts down, so if it's 0 then there's a concurrent execution happening.
             if (logConcurrentExecution && semaphore.CurrentCount == 0)
             {
@@ -46,8 +44,6 @@ namespace Nevermore.Advanced
 
         public async Task WaitAsync(CancellationToken cancellationToken)
         {
-            AssertNoDeadlock();
-            
             // `SemaphoreSlim` counts down, so if it's 0 then there's a concurrent execution happening.
             if (logConcurrentExecution && semaphore.CurrentCount == 0)
             {
@@ -63,29 +59,6 @@ namespace Nevermore.Advanced
             threadWhichHasAcquiredLock = null;
             taskWhichHasAcquiredLock = null;
             semaphore.Release();
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        void AssertNoDeadlock()
-        {
-            if (taskWhichHasAcquiredLock is not null)
-            {
-                // If we have a task then we can rely on the task ID. It's not guaranteed (one task can still spawn another) but it's better than nothing.
-                // If it's a different task which has acquired the lock then there's at least _some_ hope that that task might complete without requiring
-                // this task to complete. If this task has the lock and is attempting to acquire it again then there's no way out - deadlock.
-                if (taskWhichHasAcquiredLock == Task.CurrentId)
-                    throw new DeadlockException("This task context has already acquired this lock and has attempted to do so again.");
-            }
-            else
-            {
-                // If we have no task then our best guess is that we're using sync-only code, which means that the thread ID _should_ be
-                // a good indicator of the call context.
-                if (threadWhichHasAcquiredLock is not null)
-                    // If this thread has already acquired a lock and it's trying to do so again then
-                    // it's very unlikely that the first lock will be released, hence deadlock.
-                    if (threadWhichHasAcquiredLock == Thread.CurrentThread.ManagedThreadId)
-                        throw new DeadlockException("This thread has already acquired this lock and has attempted to do so again.");
-            }
         }
 
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
